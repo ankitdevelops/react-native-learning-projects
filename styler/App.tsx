98a202bff3aa22202365d9465fc618486bdff489
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-14 19:34:38 +0530

    all about flexbox

--- styler/App.tsx
+++ styler/App.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import FlatCard from './Components/FlatCard';
 
-import Falt from './Falt';
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
-        <Falt />
         <FlatCard />
       </ScrollView>
     </SafeAreaView>
--- styler/Components/FlatCard.tsx
+++ styler/Components/FlatCard.tsx
@@ -1,12 +1,58 @@
-import {View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const FlatCard = () => {
   return (
     <View>
-      <Text>FlatCard</Text>
+      <Text style={styles.headingText}>Flat Card</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Green</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default FlatCard;
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    width: 100,
+    height: 100,
+    margin: 8,
+    borderRadius: 4,
+    flex: 1,
+    // display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50DBB4',
+  },
+  cardThree: {
+    backgroundColor: '#5DA3FA',
+  },
+});
