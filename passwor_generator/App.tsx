838301943303bcba0d7c6f584ffdce02e144c599
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-28 22:42:25 +0530

    logic to generate password

--- passwor_generator/App.tsx
+++ passwor_generator/App.tsx
@@ -22,7 +22,11 @@ const App = () => {
   };
 
   const createPassword = (characters: String, passwordLength: number) => {
-    //
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
   };
 
   const resetPassword = () => {
